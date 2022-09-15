fdd8de6524899170d90bde64f99d5460ddc84bba
hp0404 <39014459+hp0404@users.noreply.github.com>
2022-09-15 20:30:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    package_dir={"terms": "terms"},
     package_data={
         "terms": ["assets/*.json"],
     },
